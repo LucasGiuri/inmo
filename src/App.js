cabd548b7fc2244cef50b3407fece5773aaccc44
lucas giuri <lucasgiuri@BCN001DEV023.local>
2021-09-01 19:59:38 +0200

    small changes

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
-import React from "react";
-import "./App.css";
-import Navbar from "./components/navbar/navbar";
-import Home from "./pages/home/home";
-import Contact from "./pages/contact/contact";
-import News from "./pages/news/news";
-import Property from "./pages/property/property";
-import Properties from "./pages/properties/properties";
-import { StateProvider } from "./state";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import WhatsappLink from "./components/whatsappLink/whatsappLink";
-import Footer from "./components/footer/footer";
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import Home from './pages/home/home';
+import Contact from './pages/contact/contact';
+import News from './pages/news/news';
+import Property from './pages/property/property';
+import Properties from './pages/properties/properties';
+import { StateProvider } from './state';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import WhatsappLink from './components/whatsappLink/whatsappLink';
+import Footer from './components/footer/footer';
 
 export default function App() {
+  const rentalsRoutes = [
+    '/rentals',
+    '/rentals/:barrio/:cochera',
+    '/rentals/:barrio/:cochera/:min/:max/:tipo/:ambientes/:apto-profesional',
+  ];
+
+  const onSaleRoutes = [
+    '/on-sale',
+    '/on-sale/:barrio/:cochera',
+    '/on-sale/:barrio/:cochera/:min/:max/:tipo/:ambientes/:apto-profesional',
+  ];
+
   return (
-    <>
-      <StateProvider>
-        <Router>
-          <Navbar />
-          <Switch>
-            <Route path="/contact">
-              <Contact />
-            </Route>
-            <Route path="/news">
-              <News />
-            </Route>
-            <Route exact path="/property/:id">
-              <Property />
-            </Route>
-            <Route exact path="/on-sale">
-              <Properties id='on-sale' />
-            </Route>
-            <Route exact path="/rentals">
-              <Properties id='rentals' />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-          <WhatsappLink link="https://wa.me/+5491162991141" />
-        </Router>
-        <Footer />
-      </StateProvider>
-    </>
+    <StateProvider>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+          <Route path='/news'>
+            <News />
+          </Route>
+          <Route exact path='/property/:id'>
+            <Property />
+          </Route>
+          <Route exact path={onSaleRoutes}>
+            <Properties id='on-sale' />
+          </Route>
+          <Route exact path={rentalsRoutes}>
+            <Properties id='rentals' />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+        <WhatsappLink link='https://wa.me/+5491162991141' />
+      </Router>
+      <Footer />
+    </StateProvider>
   );
 }
--- src/components/homeSearch/homeSearch.js
+++ src/components/homeSearch/homeSearch.js
@@ -1,9 +1,9 @@
-import { React, useState } from "react";
-
-import Select from "@material-ui/core/Select";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { React, useState } from 'react';
+import { useHistory } from "react-router-dom";
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 
 import {
@@ -18,62 +18,74 @@ import {
   SearchBarRadioBtnGrp,
   StyledRadio,
   HomeSwitcher
-} from "./homeSearch.styles";
+} from './homeSearch.styles';
 
 const HomeSearch = () => {
-  const [value, setValue] = useState("on-sale");
+  const history = useHistory();
+  const [value, setValue] = useState('rentals');
 
   const handleChange = (event) => {
     setValue(event.target.value);
   };
 
+  const onSearch = () => {
+    return history.push({
+      pathname: `/${value}`,
+      state: {
+        barrio: 'true'
+      }
+    });
+  }
+
   return (
     <HomeSearchWrapper>
       <HomeSearchContainer>
         <HomeSearchHeader>
-          <SearchBarTypography variant="h3">
+          <SearchBarTypography variant='h3'>
             Queremos ayudarte a encontrar tu lugar
           </SearchBarTypography>
-          <SearchBarTypography variant="h5">
-            Desde 1938 brindamos seguridad en sus operaciones inmobiliarias{" "}
+          <SearchBarTypography variant='h5'>
+            Desde 1938 brindamos seguridad en sus operaciones inmobiliarias{' '}
           </SearchBarTypography>
         </HomeSearchHeader>
         <HomeSearchSelector>
           <SearchBarRadioBtnGrp onChange={handleChange}>
             <FormControlLabel
-              value="on-sale"
-              control={<StyledRadio color="default" />}
-              label="Comprar"
+              value='on-sale'
+              checked={value === 'on-sale'}
+              control={<StyledRadio color='default' />}
+              label='Comprar'
             />
             <FormControlLabel
-              value="rentals"
-              control={<StyledRadio color="default" />}
-              label="Alquilar"
+              value='rentals'
+              checked={value === 'rentals'}
+              control={<StyledRadio color='default' />}
+              label='Alquilar'
             />
           </SearchBarRadioBtnGrp>
-        <FormControlLabel value="cochera" control={<HomeSwitcher color="secondary" />} label="Cochera" />
+        <FormControlLabel value='cochera' control={<HomeSwitcher color='secondary' />} label='Cochera' />
         </HomeSearchSelector>
         <HomeSearchInputs>
-          <FormControl variant="outlined">
+          <FormControl variant='outlined'>
             <InputLabel>Tipo</InputLabel>
             <Select
               native
-              label="Tipo"
+              label='Tipo'
               inputProps={{
-                name: "tipo",
+                name: 'tipo',
               }}
             >
-              <option aria-label="None" value="" />
-              <option value={""}>DEPARTAMENTO</option>
-              <option value={""}>TIPO CASA PH</option>
-              <option value={""}>LOCAL</option>
-              <option value={""}>OFICINA</option>
-              <option value={""}>TERRENO</option>
-              <option value={""}>COCHERA</option>
+              <option aria-label='None' value='' />
+              <option value={''}>DEPARTAMENTO</option>
+              <option value={''}>TIPO CASA PH</option>
+              <option value={''}>LOCAL</option>
+              <option value={''}>OFICINA</option>
+              <option value={''}>TERRENO</option>
+              <option value={''}>COCHERA</option>
             </Select>
           </FormControl>
-          <HomeSearchTextField color="danger" label="Barrio" />
-          <SearchBarButton>Buscar</SearchBarButton>
+          <HomeSearchTextField color='danger' label='Barrio' />
+          <SearchBarButton onClick={onSearch}>Buscar</SearchBarButton>
         </HomeSearchInputs>
       </HomeSearchContainer>
     </HomeSearchWrapper>
